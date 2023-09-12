a3740b3d884cc5277fdd90e0c4abb7d043b02b43
Gerjan Wielink <44892924+GerjanWielink@users.noreply.github.com>
2023-09-12 13:04:36 +0200

    fetch more complete album data

--- build/parsers.js
+++ build/parsers.js
@@ -10,17 +10,37 @@ const parseAlbumPage = (html) => {
     const imgUrl = c('img.album-cover-img').attr('src');
     const summary = c('p.static-album--description--summary').text();
     const shares = [];
-    const reviews = c('#album-reviews')
-        .find('a.album--review--link');
-    reviews.each((idx) => {
-        shares.push(reviews.eq(idx).attr('href'));
+    // const reviews = c('#album-reviews')
+    //     .find('a.album--review--link')
+    //
+    // reviews.each((idx) => {
+    //     shares.push(reviews.eq(idx).attr('href')!!)
+    // })
+    const genres = [];
+    const genreElements = c(".static-album--meta")
+        .find('dd.m-0');
+    genreElements.each(idx => {
+        genres.push(genreElements.eq(idx).text());
     });
+    const avg_rating = parseFloat(c(".static-album--meta").find('div.h4').eq(0).text());
+    const nr_of_votes = parseInt(c(".static-album--meta").find('div.h4').eq(1).text(), 10);
+    const spotify = c('a[aria-label=Spotify]').attr('href');
+    const youtube = c('a[aria-label=Youtube]').attr('href');
+    const apple_music = c('a[aria-label*=Apple]').attr('href');
+    const wikipedia = c('a[aria-label=Wikipedia]').attr('href');
     return {
         title,
         artist,
         year,
         imgUrl,
         summary,
+        genres,
+        avg_rating,
+        nr_of_votes,
+        spotify,
+        youtube,
+        apple_music,
+        wikipedia,
         shares: shares
     };
 };
--- src/parsers.ts
+++ src/parsers.ts
@@ -10,14 +10,28 @@ export const parseAlbumPage = (html: string) => {
     const summary = c('p.static-album--description--summary').text()
 
     const shares : string[] = []
-    const reviews = c('#album-reviews')
-        .find('a.album--review--link')
+    // const reviews = c('#album-reviews')
+    //     .find('a.album--review--link')
+    //
+    // reviews.each((idx) => {
+    //     shares.push(reviews.eq(idx).attr('href')!!)
+    // })
 
-    reviews.each((idx) => {
-        shares.push(reviews.eq(idx).attr('href')!!)
+    const genres : string[] = []
+    const genreElements = c(".static-album--meta")
+        .find('dd.m-0')
+
+    genreElements.each (idx => {
+        genres.push(genreElements.eq(idx).text())
     })
 
+    const avg_rating = parseFloat(c(".static-album--meta").find('div.h4').eq(0).text())
+    const nr_of_votes = parseInt(c(".static-album--meta").find('div.h4').eq(1).text(), 10)
 
+    const spotify = c('a[aria-label=Spotify]').attr('href')
+    const youtube = c('a[aria-label=Youtube]').attr('href')
+    const apple_music = c('a[aria-label*=Apple]').attr('href')
+    const wikipedia = c('a[aria-label=Wikipedia]').attr('href')
 
     return {
         title,
@@ -25,6 +39,13 @@ export const parseAlbumPage = (html: string) => {
         year,
         imgUrl,
         summary,
+        genres,
+        avg_rating,
+        nr_of_votes,
+        spotify,
+        youtube,
+        apple_music,
+        wikipedia,
         shares: shares
 
     }
